4fb426e1bf679dc45ab459fe63539617596cd22e
Jezabel <jezabel79@hotmail.es>
2023-04-24 15:40:53 +0200

    Primera historia usuario completa

--- src/main/kotlin/com/factoriafilmscity/factoriafilmscity/repositories/Movie.kt
+++ src/main/kotlin/com/factoriafilmscity/factoriafilmscity/repositories/Movie.kt
@@ -2,13 +2,18 @@ package com.factoriafilmscity.factoriafilmscity.repositories
 
 import jakarta.persistence.*
 
+
 @Table(name = "movies")
 @Entity
 data class Movie(
         var title: String,
+        var coverImage: String,
+        var director: String,
+        var synopsis: String,
+        var releaseYear: Int,
 
-       @Id
-       @GeneratedValue(strategy = GenerationType.IDENTITY)
-       @Column(name = "id", nullable = false)
-       var id: Long? = null
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id", nullable = false)
+        var id: Long? = null
 )
